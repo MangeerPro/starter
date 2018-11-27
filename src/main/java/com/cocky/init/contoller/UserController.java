2902fa0d4816b4c76382a2e91a2bdf054e4d908c
cocky <smile.xiaohao@gmail.com>
2018-11-28 02:18:58 +0800

    update 20181128

--- src/main/java/com/cocky/init/contoller/UserController.java
+++ src/main/java/com/cocky/init/contoller/UserController.java
@@ -1,23 +1,30 @@
 package com.cocky.init.contoller;
 
+import java.util.List;
+
 import com.cocky.init.po.User;
 import com.cocky.init.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger logger = 
+                    LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
 
     @RequestMapping("query")
     public List<User> query() {
-        return userService.getUsers();
+        List<User> users = userService.getUsers();
+        logger.info(users.toString());
+        return users;
     }
 
     @RequestMapping("add")
